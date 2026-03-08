29449ab6024c8c10dac524d4b3dc25b96dbfb0e0
Matt Rinsch <gfunkmonk@mailbox.org>
2026-03-08 06:22:31 -0400

    lol

--- vim-static-musl.sh
+++ vim-static-musl.sh
@@ -107,10 +107,10 @@ upx \
 python3-dev \
 perl-dev \
 perl && \
-wget "https://github.com/gfunkmonk/vim-static-musl/raw/refs/heads/main/tiny-vim.patch" && \
+wget "https://github.com/gfunkmonk/vim-static-musl/raw/refs/heads/main/disable-error.patch" && \
 tar xf vim-${VIM_VERSION}.tar.gz && \
 cd vim-${VIM_VERSION}/ && \
-patch -p1 --fuzz=4 < ../tiny-vim.patch && \
+patch -p1 --fuzz=4 < ../disable-error.patch && \
 ./configure CC='gcc' --disable-channel --disable-gpm --disable-gtktest --disable-gui --disable-netbeans --disable-nls --disable-selinux --disable-smack --disable-sysmouse --disable-xsmp --enable-multibyte --with-features=huge --with-tlib=ncursesw --without-x LDFLAGS='-static' CFLAGS='-Os -static -fno-stack-protector -no-pie' && \
 CC='gcc' make -j\$(nproc) && \
 strip src/vim && \
